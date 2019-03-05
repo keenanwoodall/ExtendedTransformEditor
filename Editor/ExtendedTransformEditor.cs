b99018266bfeea10cfbc24066422a8a8e6b80641
keenanwoodall <keenanwoodall@gmail.com>
2019-03-05 05:01:05 -0600

    Added reset buttons

--- Editor/ExtendedTransformEditor.cs
+++ Editor/ExtendedTransformEditor.cs
@@ -8,11 +8,28 @@ namespace ExtendedTransformInspector
 	{
 		private class Content
 		{
+			public static Texture2D ResetTexture = Resources.Load<Texture2D> (EditorGUIUtility.isProSkin ? "Textures/ETI_Pro_Reset" : "Textures/ETI_Personal_Reset");
+
 			public static readonly GUIContent Position	= new GUIContent ("Position", "The local position of this GameObject relative to the parent.");
 			public static readonly GUIContent Rotation	= new GUIContent ("Rotation", "The local rotation of this Game Object relative to the parent.");
 			public static readonly GUIContent Scale		= new GUIContent ("Scale", "The local scaling of this GameObject relative to the parent.");
+			public static readonly GUIContent ResetPosition = new GUIContent (ResetTexture, "Reset the position.");
+			public static readonly GUIContent ResetRotation = new GUIContent (ResetTexture, "Reset the rotation.");
+			public static readonly GUIContent ResetScale = new GUIContent (ResetTexture, "Reset the scale.");
 
 			public const string FloatingPointWarning = "Due to floating-point precision limitations, it is recommended to bring the world coordinates of the GameObject within a smaller range.";
+		}
+
+		private class Styles
+		{
+			public static GUISkin Skin;
+			public static GUIStyle ResetButton;
+
+			static Styles ()
+			{
+				Skin = Resources.Load<GUISkin> ("ETI");
+				ResetButton = Skin.GetStyle ("button");
+			}
 		}
 
 		private class Properties
@@ -54,9 +71,34 @@ namespace ExtendedTransformInspector
 
 			serializedObject.UpdateIfRequiredOrScript ();
 
-			EditorGUILayout.PropertyField (properties.Position, Content.Position);
-			rotationGUI.Draw ();
-			EditorGUILayout.PropertyField (properties.Scale, Content.Scale);
+			using (new EditorGUILayout.HorizontalScope ())
+			{
+				EditorGUILayout.PropertyField (properties.Position, Content.Position);
+				if (GUILayout.Button (Content.ResetPosition, Styles.ResetButton))
+					properties.Position.vector3Value = Vector3.zero;
+			}
+			using (new EditorGUILayout.HorizontalScope ())
+			{
+				rotationGUI.Draw ();
+				if (GUILayout.Button (Content.ResetRotation, Styles.ResetButton))
+				{
+					properties.Rotation.quaternionValue = Quaternion.identity;
+					if (Tools.current == Tool.Rotate)
+					{
+						if (Tools.pivotRotation == PivotRotation.Global)
+						{
+							Tools.handleRotation = Quaternion.identity;
+							SceneView.RepaintAll ();
+						}
+					}
+				}
+			}
+			using (new EditorGUILayout.HorizontalScope ())
+			{
+				EditorGUILayout.PropertyField (properties.Scale, Content.Scale);
+				if (GUILayout.Button (Content.ResetScale, Styles.ResetButton))
+					properties.Scale.vector3Value = Vector3.one;
+			}
 
 			serializedObject.ApplyModifiedProperties ();
 
