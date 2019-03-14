087b13e958c91c547c6f2843c8ea8dc229b0ad39
mandarinx <mandarin@mandarin.no>
2019-03-14 12:49:32 +0100

    Encoded icons as base64 strings
    To get rid of the Editor Default Resources folder

--- Editor/ExtendedTransformEditor.cs
+++ Editor/ExtendedTransformEditor.cs
@@ -8,7 +8,7 @@ namespace Beans.Unity.ETE
 	{
 		private class Content
 		{
-			public static Texture2D ResetTexture = LoadResource<Texture2D>(EditorGUIUtility.isProSkin ? "Textures/ETE_Pro_Reset.png" : "Textures/ETE_Personal_Reset.png");
+			public static Texture2D ResetTexture = EditorGUIUtility.isProSkin ? CreateTexture2D(Styles.iconWidth, Styles.iconHeight, Styles.iconResetPro) : CreateTexture2D(Styles.iconWidth, Styles.iconHeight, Styles.iconResetPersonal);
 
 			public static readonly GUIContent Position	= new GUIContent ("Position", "The local position of this GameObject relative to the parent.");
 			public static readonly GUIContent Rotation	= new GUIContent ("Rotation", "The local rotation of this Game Object relative to the parent.");
@@ -18,15 +18,25 @@ namespace Beans.Unity.ETE
 			public static readonly GUIContent ResetScale = new GUIContent (ResetTexture, "Reset the scale.");
 
 			public const string FloatingPointWarning = "Due to floating-point precision limitations, it is recommended to bring the world coordinates of the GameObject within a smaller range.";
+
+            private static Texture2D CreateTexture2D(int width, int height, string base64) {
+                Texture2D tex = new Texture2D(width, height);
+                tex.LoadImage(System.Convert.FromBase64String(base64));
+                return tex;
+            }
 		}
 
 		private class Styles
 		{
 			public static GUIStyle ResetButton;
+			public const int iconWidth = 15;
+			public const int iconHeight = 15;
+			public static readonly string iconResetPersonal = "iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAI/SURBVFhH7ZdLq05RGIA/9zoxYUDhmGCmMBBSlB/gkt/gHyhEkdxSSpkbSX6A20CSGblnwBwZMHArd89z2u9pddp7n7W/vQ6T76mn1be+s9/v3Wu/6137DEaM6MmsahyGRbgJ1+BiJ+ADvsL7+NmJmWY27sNb+AP/NOh3N3Ev9rnpVrbhc6xLoM2nuBWzyM3+CB5HVyj4jU8q3zkBS3EDrsc09i88iqcnPvXkIqZ3/BFP4ApsYiWexE+YXnsBe+HKpAFv43LMxaTvYBrjIA6FNeNSR6DLOBe7Mg+vYMT5iZuxE9ZKWsCuzDDJBCZ1FyPeY+y0+9zacbE10+UxNWFd2Zsi7h7Mxj4TF1rApTiFEfe6EznYgaPpWUNtu6kr42i7MPZ3HMNp2YlxFw+daGFBNXbBvhXxdziRkja6wLMp8OI2vlVjFyzoYG01TlKXUByUEh24JG+rUZZU4yQmdAxjCdUOGxzC9Dv/ti9Z231qUnWWSEYuYcTc70QTbUk1JTO/Grvg6R9xtzvRRl1SpVZGVmFsezdE1rZPkyqZjJzBiH3NiVxMpHQyrs4XjIR24X/Dw/UeRjI23Bl7tZ0Ok7mKkYzHkv8cFGE1eh7l4mNKV0YPYBE8Ul6jdXAW/bEm/M4CTmtGz2Mruc9xIb7AdHX8gWf4CN/gHFyGG3EdprF9SzyM5yY+FWILejCmd5yjBVysZqbiXe/GG+j7TF0CatOzz7i1O+2mPlvPx+id+wrhqW0Hfo8v8QF+xREj/jGDwV/bjcvhi8awDAAAAABJRU5ErkJggg==";
+			public static readonly string iconResetPro = "iVBORw0KGgoAAAANSUhEUgAAACQAAAAkCAYAAADhAJiYAAAAAXNSR0IArs4c6QAAAARnQU1BAACxjwv8YQUAAAAJcEhZcwAADsMAAA7DAcdvqGQAAAKESURBVFhH7ZfLahRBFIano27EbHShYEw2xp2gLkRFUPABYiTP4BsIKglEQrwgCIJ7VyI+QLwsRMSdkosRF7pXcaELEwU1yfj9VaeGwpme7p6uZDUffBR9qvr0me6q6p5Gnz41yaytTLPZHKQ5jqO4WzH4jh/xdZZlqy6ymVDEAE7gM/yLeajvKV7Ann90V0h8Gt9hVd7iKUtTSKnqSThJcw0HXMCzgUvmVwVgLx7FIxjnXscpHuMNf1gDirmnnxnxA2dwyIa0Qd8BnMUVjLlrQ3qDBJM+T4vnuN+6C2HsEL7QiRGXrbsanKg5s+5SeB7gdusuDefswIdKYKzhCesuBydoNcUTWHemcjEBzlVRL5XIWMTyq4/BWtoBzZnSjykPcmherSqhMW5dxTBY+0xgxsK1Idd1n9Lx2MLdYeAghk1Pcyh3NVWFXMO4ocTwB3daVz4MOueGe+YtnAxyLvnUjrMWbhFvdAG9mwLa9FKzaK04ZG2LTgWFF6UIO3BKvlgr9ljbQst72t89D7FZ3+W4YuHAtMXrULzcdSF/va6kKEbXuu/TOS5auB06uxWVpBhBLr39A2cs3BkGdCoqZTEjGJb9byy17OOikhUjyHfTp3XMWbgYBquo1MXo7vzEwJh1bT1cXC/XV64MzzxuzqdtEVxYxTxSFYZeS/pzUB8SHcRhOyyEsXpM8Z0Rl6y7HiQaxU+oeXALR6yrDfWhJnA8Z8QdG5JL2Y/8XTTvMb472tWXcQE/4zbch8fwMMa51/AqH/m3/WECKOok6kuvKprAaebM/5A4w/P4BPU9k4c2vTkcw0qrqeelx4X0GPXL9Qmht7b+p33DD/iGx/OLtk+fLabR+AeZYZ4+2Q2vaAAAAABJRU5ErkJggg==";
 
 			static Styles ()
 			{
-                ResetButton = new GUIStyle { fixedWidth = 15, fixedHeight = 15, margin = new RectOffset(0, 0, 2, 0) };
+                ResetButton = new GUIStyle { fixedWidth = iconWidth, fixedHeight = iconHeight, margin = new RectOffset(0, 0, 2, 0) };
 			}
 		}
 
